9caf5be1a8edf7c20456739ba8f062f48f565964
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-11-17 20:37:29 +0200

    working app

--- application/Application.java
+++ application/Application.java
@@ -1,11 +1,10 @@
 package application;
 
-import core.CacmDocument;
-import core.CacmDocumentList;
+import core.lists.CacmDocumentList;
+import core.lists.CacmQueryList;
 import io.XmlReader;
 import io.XmlWriter;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import parsers.CacmDocParser;
@@ -14,12 +13,12 @@ import parsers.CacmQueryParser;
 public class Application {
 
 	public static void main(String[] args) {
-		if (args.length != 2) {
-			throw new IllegalArgumentException("Wrong argument count");
-		}
 		try {
-			CacmDocumentList txtlist = parseDocs(args[0]);
-			parseQueries(args[1]);
+			if (args.length != 2) {
+				throw new IllegalArgumentException("Wrong argument count");
+			}
+			CacmDocumentList doclist = parseDocs(args[0]);
+			CacmQueryList querylist = parseQueries(args[1]);
 		} catch (Exception ex) {
 			Logger.getLogger(Application.class.getName()).log(Level.SEVERE, "Fatal Error: ", ex);
 			System.exit(1);
@@ -28,13 +27,12 @@ public class Application {
 
 	private static CacmDocumentList parseDocs(String docfile) throws IOException, Exception {
 		CacmDocParser docParser = new CacmDocParser(docfile);
-		List<CacmDocument> documents = docParser.parse();
-		return new CacmDocumentList(documents);
+		return new CacmDocumentList(docParser.parse());
 	}
 
-	private static void parseQueries(String queriesfile) throws IOException {
+	private static CacmQueryList parseQueries(String queriesfile) throws IOException {
 		CacmQueryParser queryParser = new CacmQueryParser(queriesfile);
-		queryParser.parse();
+		return new CacmQueryList(queryParser.parse());
 	}
 
 	private static void writeCacmXmlDocument(CacmDocumentList cacmDocumentList, String xmlfile) throws Exception {
